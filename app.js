6bb500bc6caf2608d155e3035997f7413890debc
vincent <vincent.ledu@gmail.com>
2018-09-27 00:11:49 +0200

    nothing to say...

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
+const fs = require('fs');
 const express = require('express')
 const app = express()
+const session = require('cookie-session'); // Charge le middleware de sessions
+const bodyParser = require('body-parser'); // Charge le middleware de gestion des paramètres
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const morgan = require('morgan'); // Charge le middleware de logging
+const favicon = require('serve-favicon'); // Charge le middleware de favicon
+//const multer  = require('multer')
+//const JsonDB = require('node-json-db');
+//const Jimp = require("jimp");
+
+
+app.use(session({ secret: 'calendarsessionsecret' }))
+.use(morgan('combined'))
+.use(express.static(__dirname + '/public'))
+.use(favicon(__dirname + '/public/favicon.png'));
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
+})
+
+app.get('/apartments/', function (req, res) {
+  res.render('appartments.ejs')
+})
+
+app.post('/apartments/', function(req, res, next) {
+  if (req.body.title != 'undefined' && req.body.title != '') {
+    var title = req.body.title
+  }
+  else {
+    console.log("title is not defined")
+  }
+  var description = req .body.description
+  console.log()
+  res.redirect('/apartments')
 })
 
 app.listen(3000, function () {
